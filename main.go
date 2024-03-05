fd566f853bd6e89b6cacd05910cb3f4d97fc3a75
fasnow <>
2024-03-05 22:41:52 +0800

    -

--- main.go
+++ main.go
@@ -20,12 +20,6 @@ func main() {
 		push.GetDingTalkSingleton(),
 	)
 
-	//设置代理
-	err := proxy.GetSingleton().SetProxy("http://127.0.0.1:8080")
-	if err != nil {
-		logger.Info(err.Error())
-	}
-
 	waitGroup := &sync.WaitGroup{}
 	waitGroup.Add(1)
 	logger.Info("service is running...")
