8e4cfaa5daf0abbc6498eef3d066801ccf8a327f
fasnow <>
2024-03-07 14:04:12 +0800

    添加阿里云漏洞推送

--- main.go
+++ main.go
@@ -1,20 +1,69 @@
 package main
 
 import (
+	"cveHunter/config"
+	"cveHunter/logger"
+	"cveHunter/monitor"
+	"cveHunter/proxy"
+	"cveHunter/push"
 	"fmt"
-	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
 
 func main() {
-	// HTML 中的 Unicode 字符
-	htmlString := `Teamcity \u003c 2023.11.4`
+	//初始化配置文件
+	configSingleton := config.GetSingleton()
 
-	// 解码 HTML 中的转义字符
-	unquotedString, err := strconv.Unquote(`"` + htmlString + `"`)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+	pusherList := []string{}
+	if configSingleton.DingTalk.Enable {
+		pusherList = append(pusherList, "DingTalk")
+	}
+	if configSingleton.LarkAssistant.Enable {
+		pusherList = append(pusherList, "LarkAssistant")
+	}
+	if configSingleton.LarkBot.Enable {
+		pusherList = append(pusherList, "LarkBot")
+	}
+	if len(pusherList) == 0 {
+		logger.Info("didn't enable any pusher,won't push any event")
+	} else {
+		logger.Info(fmt.Sprintf("enabled pusher [%s]", strings.Join(pusherList, ", ")))
 	}
 
-	fmt.Println("Unquoted string:", unquotedString)
+	//使用代理的模块
+	proxy.GetSingleton().Add(
+		monitor.GetGithubSingleton(),
+		monitor.GetAVDMonitorSingleton(),
+		push.GetDingTalkSingleton(),
+		push.GetLarkAssistantLarkSingleton(),
+		push.GetLarkSingleton(),
+	)
+
+	waitGroup := &sync.WaitGroup{}
+	logger.Info("service is running...")
+	waitGroup.Add(1)
+	go func() {
+		defer waitGroup.Done()
+		if config.GetSingleton().Github.Enable {
+			for {
+				RunGithubMonitor()
+				logger.Info("waiting for next loop...")
+				time.Sleep(configSingleton.Base.Interval)
+			}
+		}
+	}()
+	waitGroup.Add(1)
+	go func() {
+		defer waitGroup.Done()
+		if config.GetSingleton().AVD.Enable {
+			for {
+				RunAVDMonitor()
+				logger.Info("waiting for next loop...")
+				time.Sleep(configSingleton.Base.Interval)
+			}
+		}
+	}()
+	waitGroup.Wait()
 }
